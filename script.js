c79198115ae3434b483bce39a5ca60f5351c9dfb
adramelech <adramelech.psd@gmail.com>
2021-08-09 11:08:44 +0530

    implemented clearInterval to readjust the interval function to manual operation and also pause and resume on hover implemented

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 const imageArray = [
-  "https://source.unsplash.com/1600x900/?cars",
-  "https://source.unsplash.com/1600x900/?Women",
-  "https://source.unsplash.com/1600x900/?tech",
-  "https://source.unsplash.com/1600x900/?model",
-  "https://source.unsplash.com/1600x900/?nature",
-  "https://source.unsplash.com/1600x900/?grocery",
-  "https://source.unsplash.com/1600x900/?hill",
-  "https://source.unsplash.com/1600x900/?man",
-  "https://source.unsplash.com/1600x900/?fashion",
+	"https://source.unsplash.com/1600x900/?cars",
+	"https://source.unsplash.com/1600x900/?Women",
+	"https://source.unsplash.com/1600x900/?tech",
+	"https://source.unsplash.com/1600x900/?model",
+	"https://source.unsplash.com/1600x900/?nature",
+	"https://source.unsplash.com/1600x900/?grocery",
+	"https://source.unsplash.com/1600x900/?hill",
+	"https://source.unsplash.com/1600x900/?man",
+	"https://source.unsplash.com/1600x900/?fashion",
 ];
 
 // ------------------Adding images dynamically from array to DOM --------------
 
 const container = document.querySelector(".container");
 (function () {
-  imageArray.forEach((image) => {
-    container.innerHTML += `<div class="img">
+	imageArray.forEach((image) => {
+		container.innerHTML += `<div class="img">
           <img src=${image} alt="" />
         </div>`;
-  });
+	});
 })();
-// addImages();
 
 // --------------------------END--------------------------------------
 
@@ -28,55 +27,71 @@ const rightBtn = document.getElementById("btn-right");
 const leftBtn = document.getElementById("btn-left");
 const images = document.querySelectorAll(".img");
 const heroBtns = document.querySelector(".herobtns");
+const delay = 6000;
 let currentImageIndex = 0;
+let interval;
+
+// --------------------Checking if the currentImageIndex is zero or end off array ------------------------------
+
+const checkIndex = () => {
+	if (currentImageIndex >= images.length) {
+		currentImageIndex = 0;
+	} else if (currentImageIndex < 0) {
+		currentImageIndex = images.length - 1;
+	}
+};
 
 // --------------------------Slider Function--------------------------
 
 const slide = () => {
-  if (currentImageIndex >= images.length) {
-    currentImageIndex = 0;
-  } else if (currentImageIndex < 0) {
-    currentImageIndex = images.length - 1;
-  }
-  let a = images.forEach((image, index) => {
-    if (index === currentImageIndex) {
-      image.classList.add("show-img");
-      heroBtns.children[currentImageIndex].classList.add("fas");
-      heroBtns.children[currentImageIndex].classList.remove("far");
-    } else {
-      image.classList.remove("show-img");
-      heroBtns.children[index].classList.remove("fas");
-      heroBtns.children[index].classList.add("far");
-    }
-  });
+	checkIndex();
+	clearInterval(interval);
+	images.forEach((image, index) => {
+		if (index === currentImageIndex) {
+			image.classList.add("show-img");
+			heroBtns.children[currentImageIndex].classList.add("fas");
+			heroBtns.children[currentImageIndex].classList.remove("far");
+		} else {
+			image.classList.remove("show-img");
+			heroBtns.children[index].classList.remove("fas");
+			heroBtns.children[index].classList.add("far");
+		}
+	});
+	interval = setInterval(() => {
+		currentImageIndex++;
+		slide();
+	}, delay);
 };
 
 // -----------------Herobtn invoked function to go to a specific index-----------------
 
 const gotoSpecific = (e) => {
-  let index = e.target.id.split("_");
-  currentImageIndex = Number(index[1]);
-  slide();
+	clearInterval(interval);
+	let index = e.target.id.split("_");
+	currentImageIndex = Number(index[1]);
+	slide();
 };
 
 // ---------------------------Right Slide-----------------------------
 
 const right = () => {
-  currentImageIndex++;
-  slide();
+	clearInterval(interval);
+	currentImageIndex++;
+	slide();
 };
 
 // --------------------------------Left slide----------------------------
 
 const left = () => {
-  currentImageIndex--;
-  slide();
+	clearInterval(interval);
+	currentImageIndex--;
+	slide();
 };
 
 // --------------------Adding Hero btns to DOM---------------------
 
 images.forEach((image, index) => {
-  heroBtns.innerHTML += `<i class="far fa-circle" id="herobtn_${index}"></i>`;
+	heroBtns.innerHTML += `<i class="far fa-circle" id="herobtn_${index}"></i>`;
 });
 
 // ------------Initialising first image as default---------------------
@@ -86,9 +101,7 @@ slide();
 // -----------------------Auto slide---------------------------
 
 window.addEventListener("load", () => {
-  setInterval(() => {
-    right();
-  }, 6000);
+	setTimeout(right, delay);
 });
 
 // ------------------ Event Listeners -------------------------
@@ -96,3 +109,7 @@ window.addEventListener("load", () => {
 leftBtn.addEventListener("click", left);
 rightBtn.addEventListener("click", right);
 heroBtns.addEventListener("click", gotoSpecific);
+container.addEventListener("mouseover", () => {
+	clearInterval(interval);
+});
+container.addEventListener("mouseleave", right);
